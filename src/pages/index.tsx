4e25ba11be5c8a3e662ead88e2f73df2a4a12e6b
piyapat <piyapat.thongrueng@gmail.com>
2026-02-18 16:59:27 +0700

    feat: integrate axios for testing API server

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,50 @@
-export default function Home() {
-  return (
-    <div className="flex justify-center flex-col items-center space-y-5">
-      <h1 className="font-bold text-center mt-10 headline-1 text-blue-500">
-        Welcome to HomeService Test Deployment
-      </h1>
-      <button className="btn-primary">Button Primary</button>
-      <button className="btn-primary" disabled>
-        Button Primary
-      </button>
-      <button className="btn-primary">
-        Processing
-        <span className="spinner"></span>
-      </button>
-      <button className="btn-secondary">Button Secondary</button>
-      
-      <button className="btn-secondary" disabled>
-        Button Secondary
-      </button>
+import axios from "axios";
+import { useState } from "react";
 
-      <button className="btn-secondary">
-        Processing
-        <span className="spinner"></span>
-      </button>
-    </div>
+export default function Home() {
+  const [text, settext] = useState("");
+
+  const fetchTestApi = async () => {
+    try {
+      const response = await axios.get(
+        "https://homeservice-server.vercel.app/test",
+      );
+      settext(response.data.message);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  return (
+    <>
+      <div className="flex justify-center flex-col items-center space-y-5">
+        <h1 className="font-bold text-center mt-10 headline-1 text-blue-500">
+          Welcome to HomeService Test Deployment
+        </h1>
+        <button className="btn-primary" onClick={fetchTestApi}>
+          Button Primary
+        </button>
+        <div>
+          <p className="text-center">{text}</p>
+        </div>
+        <button className="btn-primary" disabled>
+          Button Primary
+        </button>
+        <button className="btn-primary">
+          Processing
+          <span className="spinner"></span>
+        </button>
+        <button className="btn-secondary">Button Secondary</button>
+
+        <button className="btn-secondary" disabled>
+          Button Secondary
+        </button>
+
+        <button className="btn-secondary">
+          Processing
+          <span className="spinner"></span>
+        </button>
+      </div>
+    </>
   );
 }
